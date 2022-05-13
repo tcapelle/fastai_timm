dd4f6dc8fe957c0b5210bb0e89e7b9cdd0a7cb43
Thomas Capelle <thomas.capelle@steady-sun.com>
2022-05-13 13:33:56 +0000

    new sweep with jeremy sugs

--- fine_tune.py
+++ fine_tune.py
@@ -1,59 +1,83 @@
 import wandb
 import timm
 import argparse
+import torchvision
 from fastai.vision.all import *
 from fastai.callback.wandb import WandbCallback
-from torchvision import models
+
+WANDB_PROJECT = 'fine_tune_timm'
+
+config_defaults = SimpleNamespace(
+    batch_size=64,
+    epochs=5,
+    num_experiments=3,
+    learning_rate=2e-3,
+    img_size=224,
+    resize_method="crop",
+    model_name="resnet34",
+    concat_pool=False,
+    seed=42,
+    force_torchvision=False,
+    wandb_project=WANDB_PROJECT,
+    split_func="default",
+)
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--batch_size', type=int, default=64)
-    parser.add_argument('--epochs', type=int, default=5)
-    parser.add_argument('--num_experiments', type=int, default=1)
-    parser.add_argument('--learning_rate', type=float, default=0.002)
-    parser.add_argument('--img_size', type=int, default=224)
-    parser.add_argument('--model_name', type=str, default='resnet18')
-    parser.add_argument('--seed', type=int, default=42)
-    parser.add_argument('--mixup', action='store_true')
+    parser.add_argument('--batch_size', type=int, default=config_defaults.batch_size)
+    parser.add_argument('--epochs', type=int, default=config_defaults.epochs)
+    parser.add_argument('--num_experiments', type=int, default=config_defaults.num_experiments)
+    parser.add_argument('--learning_rate', type=float, default=config_defaults.learning_rate)
+    parser.add_argument('--img_size', type=int, default=config_defaults.img_size)
+    parser.add_argument('--resize_method', type=str, default=config_defaults.resize_method)
+    parser.add_argument('--model_name', type=str, default=config_defaults.model_name)
+    parser.add_argument('--split_func', type=str, default=config_defaults.split_func)
+    parser.add_argument('--concat_pool', action='store_true')
+    parser.add_argument('--seed', type=int, default=config_defaults.seed)
     parser.add_argument('--force_torchvision', action='store_true')
-    parser.add_argument('--wandb_project', type=str, default='fine_tune_timm')
+    parser.add_argument('--wandb_project', type=str, default=WANDB_PROJECT)
     return parser.parse_args()
 
-def get_dls(batch_size, img_size, seed):
+def get_gpu_mem(device=0):
+    "Memory usage in GB"
+    gpu_mem = torch.cuda.memory_stats_as_nested_dict(device=device)
+    return (gpu_mem["reserved_bytes"]["small_pool"]["peak"] + gpu_mem["reserved_bytes"]["large_pool"]["peak"])*1024**-3
+                                     
+def get_pets(batch_size, img_size, seed, method="crop"):
     dataset_path = untar_data(URLs.PETS)
     files = get_image_files(dataset_path/"images")
-    pat = re.compile('(^[a-zA-Z]+_*[a-zA-Z]+)')
-    labels = [pat.match(f.name)[0] for f in files]
     dls = ImageDataLoaders.from_name_re(dataset_path, files, 
                                         r'(^[a-zA-Z]+_*[a-zA-Z]+)', 
                                         valid_pct=0.2, 
                                         seed=seed, 
                                         bs=batch_size,
-                                        item_tfms=Resize(img_size)) 
+                                        item_tfms=Resize(img_size, method=method)) 
     return dls
     
 
+top_5_accuracy = partial(top_k_accuracy, k=5)
     
-def train():
-    args = parse_args()
-    group_name = "torchvision" if args.force_torchvision else "timm"
-    for _ in range(args.num_experiments):
-        for model_name in timm.list_models(args.model_name):
-            print(f"Training {model_name}")
-            with wandb.init(project=args.wandb_project, group=group_name, config=args):
-                dls = get_dls(args.batch_size, args.img_size, args.seed)
-                cbs = [MixedPrecision(), WandbCallback(log_preds=False)]
-                if args.mixup: cbs.append(MixUp())
-                if args.force_torchvision: 
-                    model_name = getattr(models, model_name)
-                learn = vision_learner(dls, 
-                                       model_name, 
-                                       metrics=[accuracy, error_rate], 
-                                       cbs=cbs)
-                learn.fine_tune(args.epochs, args.learning_rate)
-
+def train(config=config_defaults):
+    print(f"\n >> Training {config.model_name}\n")
+    for _ in range(config.num_experiments):
+        with wandb.init(project=config.wandb_project, group="torchvision" if config.force_torchvision else "timm", config=config):
+            config = wandb.config
+            dls = get_pets(config.batch_size, config.img_size, config.seed, config.resize_method)
+            if config.force_torchvision: 
+                config.model_name = getattr(torchvision.models, config.model_name)
+            learn = vision_learner(dls, 
+                                   config.model_name, 
+                                   metrics=[accuracy, error_rate, top_5_accuracy], 
+                                   concat_pool=config.concat_pool,
+                                   splitter=default_split if config.split_func=="default" else None,
+                                   cbs=WandbCallback(log_preds=False)).to_fp16()
+            learn.fine_tune(config.epochs, config.learning_rate)
+            wandb.summary({"GPU_mem": get_gpu_mem(learn.dls.device)})
+    
 if __name__ == "__main__":
-    train()
+    args = parse_args()
+    train(config=args)
+    
     
 
 
--- sweep.py
+++ sweep.py
@@ -1,35 +1,35 @@
-import wandb, argparse
-from jeremy_ft import train
-
+import wandb, argparse, pandas
+from types import SimpleNamespace
+from fine_tune import train
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--sweep_id', type=str, default=None)
-    parser.add_argument('--sweep_count', type=int, default=30)
-    parser.add_argument('--sweep_method', type=str, default="bayes")
-    parser.add_argument('--sweep_goal', type=str, default="minimize")
-    parser.add_argument('--sweep_metric_name', type=str, default="error_rate")
-    parser.add_argument('--early_terminate_min_iter', type=int, default=5)
-    parser.add_argument('--early_terminate_type', type=str, default="hyperband")
-    parser.add_argument('--model_name', type=str, default="resnet34")
+    parser.add_argument('--sweep_count', type=int, default=None)
+    parser.add_argument('--sweep_method', type=str, default="grid")
+    # parser.add_argument('--sweep_goal', type=str, default="minimize")
+    # parser.add_argument('--sweep_metric_name', type=str, default="error_rate")
+    # parser.add_argument('--early_terminate_min_iter', type=int, default=5)
+    # parser.add_argument('--early_terminate_type', type=str, default="hyperband")
     return parser.parse_args()
+
+models = pandas.read_csv("timm_models.csv").model.to_list()
 
 def do_sweep():
     args = parse_args()
     sweep_configs = {
         "method": args.sweep_method,
-        "metric": {"name": args.sweep_metric_name, 
-                   "goal": args.sweep_goal},
-        "early_terminate": {
-            "type": args.early_terminate_type,
-            "min_iter": args.early_terminate_min_iter,
-        },
+        # "metric": {"name": args.sweep_metric, 
+        #            "goal": args.sweep_goal},
+        # "early_terminate": {
+        #     "type": args.early_terminate_type,
+        #     "min_iter": args.early_terminate_min_iter,
+        # },
         "parameters": {
-            "model_name": {"value": args.model_name},
-            "fit_learning_rate": {"distribution": "uniform", "min": 1e-5, "max": 1e-2},
-            "ft_learning_rate": {"distribution": "uniform", "min": 1e-5, "max": 1e-2},
-            "fit_epochs": {"values": [0,1]},
-            "split_func": {"values": ["default", "timm"]},
+            "model_name": {"values": models},
+            # "split_func": {"values": ["default", "timm"]},
+            "concat_pool": {"values": [True, False]},
+            "resize_method":{"values":["crop", "squish"]}
         },
     }
     if args.sweep_id is None:
@@ -37,6 +37,9 @@ def do_sweep():
             sweep_configs,
             project="fine_tune_timm",
         )
+    else:
+        print(f"Attaching runs to sweep: {args.sweep_id}")
+        sweep_id = "fine_tune_timm"+"/"+args.sweep_id
     wandb.agent(sweep_id, function=train, count=args.sweep_count)
 
 
