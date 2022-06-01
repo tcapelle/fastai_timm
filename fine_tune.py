7a6fb7a32c5b7448c2225261b166865677a7e3d7
Jeremy Howard <j@fast.ai>
2022-06-02 04:12:33 +1000

    format

--- fine_tune.py
+++ fine_tune.py
@@ -45,63 +45,43 @@ def get_gpu_mem(device=0):
     "Memory usage in GB"
     gpu_mem = torch.cuda.memory_stats_as_nested_dict(device=device)
     return (gpu_mem["reserved_bytes"]["small_pool"]["peak"] + gpu_mem["reserved_bytes"]["large_pool"]["peak"])*1024**-3
-                                     
+
 def get_pets(batch_size, img_size, seed, method="crop"):
     dataset_path = untar_data(URLs.PETS)
     files = get_image_files(dataset_path/"images")
-    dls = ImageDataLoaders.from_name_re(dataset_path, files, 
-                                        r'(^[a-zA-Z]+_*[a-zA-Z]+)', 
-                                        valid_pct=0.2, 
-                                        seed=seed, 
-                                        bs=batch_size,
-                                        item_tfms=Resize(img_size, method=method)) 
-    metrics = [error_rate, accuracy]
-    return dls, metrics
-    
+    dls = ImageDataLoaders.from_name_re(
+            dataset_path, files, r'(^[a-zA-Z]+_*[a-zA-Z]+)', valid_pct=0.2,
+            seed=seed, bs=batch_size, item_tfms=Resize(img_size, method=method))
+    return dls, [error_rate, accuracy]
+
 def get_planets(batch_size=64, img_size=224, seed=42, method="crop"):
     dataset_path=untar_data(URLs.PLANET_SAMPLE)
-    dls = ImageDataLoaders.from_csv(dataset_path, 
-                                    folder="train", 
-                                    csv_fname="labels.csv",
-                                    label_delim=" ",
-                                    suff=".jpg",
-                                    bs=batch_size,
-                                    seed=seed,
-                                    item_tfms=Resize(img_size, method=method))
+    dls = ImageDataLoaders.from_csv(
+            dataset_path, folder="train", csv_fname="labels.csv", label_delim=" ",
+            suff=".jpg", bs=batch_size, seed=seed, item_tfms=Resize(img_size, method=method))
     metrics = [accuracy_multi, FBetaMulti(beta=2)]
     return dls, metrics
-    
+
 def get_dataset(dataset_name, *args, **kwargs):
     if dataset_name   == "PETS":    return get_pets(*args, **kwargs)
     elif dataset_name == "PLANETS": return get_planets(*args, **kwargs)
     else: raise Exception("Dataset not found, supports: PETS or PLANETS")
-    
+
 def train(config=config_defaults):
     with wandb.init(project=config.wandb_project, group="timm", entity=config.wandb_entity, config=config):
         config = wandb.config
-        dls, metrics = get_dataset(config.dataset, 
-                          config.batch_size, 
-                          config.img_size, 
-                          config.seed, 
-                          config.resize_method)
-        learn = vision_learner(dls, 
-                               config.model_name, 
-                               metrics=metrics, 
-                               concat_pool=(config.pool=="concat"),
-                               splitter=default_split if config.split_func=="default" else None,
-                               cbs=WandbCallback(log=None, log_preds=False)).to_fp16()
+        dls, metrics = get_dataset(config.dataset, config.batch_size, config.img_size, config.seed, config.resize_method)
+        learn = vision_learner(
+                dls, config.model_name, metrics=metrics, concat_pool=(config.pool=="concat"),
+                splitter=default_split if config.split_func=="default" else None,
+                cbs=WandbCallback(log=None, log_preds=False)).to_fp16()
         ti = time.perf_counter()
         learn.fine_tune(config.epochs, config.learning_rate)
         wandb.summary["GPU_mem"] = get_gpu_mem(learn.dls.device)
         wandb.summary["model_family"] = config.model_name.split('_')[0]
         wandb.summary["fit_time"] = time.perf_counter() - ti
-    
+
 if __name__ == "__main__":
     args = parse_args()
-    for _ in range(args.num_experiments):
-        train(config=args)
-    
-    
-
-
+    for _ in range(args.num_experiments): train(config=args)
 
